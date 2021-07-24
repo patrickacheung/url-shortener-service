c63646825db87e8e6b36397f8b5dabdc27f4b2fe
patrickacheung <patrickcheungdev@gmail.com>
2021-07-23 22:45:49 -0700

    add postgres driver and pool class

---
+++ db/pool.mjs
@@ -0,0 +1,11 @@
+import Pool from 'pg';
+
+const pool = new Pool.Pool();
+
+export function asdf() {
+    console.log('asdf');
+};
+
+export function qwer() {
+    console.log('qwer');
+};
--- routes/redirect.mjs
+++ routes/redirect.mjs
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import {asdf, qwer} from '../db/pool.mjs';
+
 const router = new express.Router();
 
 router.get('/:code', (req, res) => {
   // check if shortCode exists in postgres.
+  asdf();
+  qwer();
 
   // if not return failure message + status code
 
