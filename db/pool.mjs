57443a7413ce3fc410ab458bf556d9fb14394810
patrickacheung <patrickcheungdev@gmail.com>
2021-07-23 22:56:46 -0700

    try amazon rds connection

--- db/pool.mjs
+++ db/pool.mjs
@@ -1,11 +1,20 @@
 import Pool from 'pg';
 
-const pool = new Pool.Pool();
+const pool = new Pool.Pool({
+  user: process.env.RDS_USERNAME,
+  host: process.env.RDS_HOSTNAME,
+  database: process.env.RDS_DB_NAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT
+});
 
 export function asdf() {
-    console.log('asdf');
+  pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res);
+    pool.end();
+  });
 };
 
 export function qwer() {
-    console.log('qwer');
+  console.log('qwer');
 };
