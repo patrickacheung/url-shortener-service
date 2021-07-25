138525327fa59c3fee56e6812191341bd47021f0
patrickacheung <patrickcheungdev@gmail.com>
2021-07-24 20:17:02 -0700

    update schemadeployer to create table and index

--- db/config/schemadeployer.mjs
+++ db/config/schemadeployer.mjs
@@ -3,6 +3,9 @@ import Pool from 'pg';
 import dbconnectionconfig from './config.mjs';
 
 const config = dbconnectionconfig.config;
+const createTableQuery = "CREATE TABLE IF NOT EXISTS urls (id bigserial PRIMARY KEY, " + 
+                         "shortened_url TEXT NOT NULL, original_url TEXT NOT NULL);";
+const createShortCodeIndexQuery = "CREATE INDEX IF NOT EXISTS short_url ON urls(shortened_url)";
 
 const pool = new Pool.Pool({
   user: config.username,
@@ -12,7 +15,16 @@ const pool = new Pool.Pool({
   port: config.port
 });
 
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
+try {
+  const res = await pool.query(createTableQuery);
+  console.log(res);
+  
+  const res2 = await pool.query(createShortCodeIndexQuery);
+  console.log(res2);
+
+} catch (err) {
+  console.log(err);
+} finally {
+  console.log('success');
   pool.end();
-});
+}
